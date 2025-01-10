e7701b0de59f1459e10e1f298ba256c334d06de9
D3vlin <alexisdq16@gmail.com>
2025-01-10 07:42:36 -0500

    Link vs NavLink

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+
+const routes = []
+routes.push({ to: '/', text: 'Home' })
+routes.push({ to: '/blog', text: 'Blog' })
+routes.push({ to: '/profile', text: 'Profile' })
 
 function Menu() {
     return (
-        <h1>Menu</h1>
+        <nav>
+            <ul>
+                {routes.map(route => (
+                    <li>
+                        <Link to={route.to}>{route.text}</Link>
+                    </li>
+                ))}
+                {routes.map(route => (
+                    <li>
+                        <NavLink
+                            to={route.to}
+                            className={({ isActive }) => ''}
+                            style={({ isActive }) => ({ color: isActive ? 'red' : 'blue' })}>
+                            {route.text}
+                        </NavLink>
+                    </li>
+                ))}
+            </ul>
+        </nav >
     )
 }
 
