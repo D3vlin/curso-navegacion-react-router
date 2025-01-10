93e52ed9f5e32fdb1feaf3aace3ec5e56d7cb1ad
D3vlin <alexisdq16@gmail.com>
2025-01-10 08:25:36 -0500

    using nested routes

--- src/Pages/BlogPage/index.js
+++ src/Pages/BlogPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 import { BlogLink } from '../../Components/BlogLink'
 import { blogdata } from '../../LocalData/blogdata'
 
@@ -11,6 +12,7 @@ function BlogPage() {
                     <BlogLink key={post.slug} post={post} />
                 ))}
             </ul>
+            <Outlet />
         </>
     )
 }
