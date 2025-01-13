a883f65599c7d8023a6b3bfce87c89021e4edd87
D3vlin <alexisdq16@gmail.com>
2025-01-13 06:06:40 -0500

    private and public route

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,32 +1,35 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { useAuth } from '../Auth'
 
 const routes = []
-routes.push({ to: '/', text: 'Home' })
-routes.push({ to: '/blog', text: 'Blog' })
-routes.push({ to: '/profile', text: 'Profile' })
-routes.push({ to: '/login', text: 'Login' })
-routes.push({ to: '/logout', text: 'Logout' })
+routes.push({ to: '/', text: 'Home', private: false })
+routes.push({ to: '/blog', text: 'Blog', private: false })
+routes.push({ to: '/profile', text: 'Profile', private: true })
+routes.push({ to: '/login', text: 'Login', private: false, publicOnly: true })
+routes.push({ to: '/logout', text: 'Logout', private: true })
 
 function Menu() {
+    const auth = useAuth()
+
     return (
         <nav>
             <ul>
-                {routes.map(route => (
-                    <li key={route.to}>
-                        <Link to={route.to}>{route.text}</Link>
-                    </li>
-                ))}
-                {routes.map(route => (
-                    <li key={route.to}>
-                        <NavLink
-                            to={route.to}
-                            className={({ isActive }) => ''}
-                            style={({ isActive }) => ({ color: isActive ? 'red' : 'blue' })}>
-                            {route.text}
-                        </NavLink>
-                    </li>
-                ))}
+                {routes.map(route => {
+                    if (route.publicOnly && auth.user) return null
+                    if (route.private && !auth.user) return null
+
+                    return (
+                        <li key={route.to}>
+                            <NavLink
+                                to={route.to}
+                                className={({ isActive }) => ''}
+                                style={({ isActive }) => ({ color: isActive ? 'red' : 'blue' })}>
+                                {route.text}
+                            </NavLink>
+                        </li>
+                    )
+                })}
             </ul>
         </nav >
     )
