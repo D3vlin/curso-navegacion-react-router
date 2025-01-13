bb68d62daf0e5d6f13731a7ad0a3c48b7bbc92f5
D3vlin <alexisdq16@gmail.com>
2025-01-13 06:38:20 -0500

    roles and permissions

--- src/Components/Auth/index.js
+++ src/Components/Auth/index.js
@@ -3,12 +3,16 @@ import { Navigate, useNavigate } from 'react-router-dom'
 
 const AuthContext = React.createContext()
 
+const adminList = ['Admin', 'Pepito', 'Panchito']
+
 function AuthProvider({ children }) {
     const navigate = useNavigate()
     const [user, setUser] = React.useState(null)
     
     const login = ({ username }) => {
-        setUser({ username })
+        const isAdmin = adminList.find(admin => admin === username)
+
+        setUser({ username, isAdmin })
         navigate("/profile")
     }
     
--- src/Pages/BlogPost/indez.js
+++ src/Pages/BlogPost/indez.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { blogdata } from '../../LocalData/blogdata'
+import { useAuth } from '../../Components/Auth'
 
 function BlogPost() {
     const navigate = useNavigate()
+    const auth = useAuth()
     const { slug } = useParams()
     const blogpost = blogdata.find(post => post.slug === slug)
+    const canDelete = auth.user?.isAdmin || blogpost.author === auth.user?.username
 
     const returnToBlog = () => {
         navigate('/blog')
@@ -18,6 +21,9 @@ function BlogPost() {
             <p>{blogpost.author}</p>
             <p>{blogpost.content}</p>
 
+            {canDelete && (
+                <button>Eliminar blogpost</button>
+            )}
         </>
     )
 }
